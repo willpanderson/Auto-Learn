07908ae9c7902f5f459a0fd05cdbabf30d4ca9d0
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2020-04-07 14:33:26 -0500

    edited launchactivity.java

---
+++ AutoLearnApp/app/src/main/java/com/example/auto_learn_app/LaunchActivity.java
@@ -0,0 +1,25 @@
+package com.example.auto_learn_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LaunchActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(LaunchActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}

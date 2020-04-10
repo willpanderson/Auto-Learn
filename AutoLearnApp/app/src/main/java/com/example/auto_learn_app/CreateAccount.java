0c0b08b2931ac11868ec71d4001a7b0a8c5ca1bb
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2020-04-10 10:26:07 -0500

    edit

--- AutoLearnApp/app/src/main/java/com/example/auto_learn_app/CreateAccount.java
+++ AutoLearnApp/app/src/main/java/com/example/auto_learn_app/CreateAccount.java
@@ -1,14 +1,73 @@
 package com.example.auto_learn_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CreateAccount extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    public EditText emailId, passwd;
+    Button btnSignUp;
+    TextView signIn;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.ETemail);
+        passwd = findViewById(R.id.ETpassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        signIn = findViewById(R.id.TVSignIn);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailID = emailId.getText().toString();
+                String paswd = passwd.getText().toString();
+                if (emailID.isEmpty()) {
+                    emailId.setError("Provide your Email first!");
+                    emailId.requestFocus();
+                } else if (paswd.isEmpty()) {
+                    passwd.setError("Set your password");
+                    passwd.requestFocus();
+                } else if (emailID.isEmpty() && paswd.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(emailID.isEmpty() && paswd.isEmpty())) {
+                    firebaseAuth.createUserWithEmailAndPassword(emailID, paswd).addOnCompleteListener(MainActivity.this, new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this.getApplicationContext(),
+                                        "SignUp unsuccessful: " + task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(MainActivity.this, UserActivity.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(MainActivity.this, ActivityLogin.class);
+                startActivity(I);
+            }
+        });
     }
 }
--- AutoLearnApp/app/src/main/java/com/example/auto_learn_app/LoginActivity.java
+++ AutoLearnApp/app/src/main/java/com/example/auto_learn_app/LoginActivity.java
@@ -1,34 +1,91 @@
 package com.example.auto_learn_app;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-
-public class LoginActivity extends AppCompatActivity {
-
-    private EditText inputEmail, inputPassword;
-    private Button btnSignIn, btnSignUp, btnResetPassword;
-    private FirebaseAuth auth;
+public class ActivityLogin extends AppCompatActivity {
+    private EditText loginEmailId, logInpasswd;
+    private Button btnLogIn;
+    private TextView signup;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginEmailId = findViewById(R.id.loginEmail);
+        logInpasswd = findViewById(R.id.loginpaswd);
+        btnLogIn = findViewById(R.id.btnLogIn);
+        signup = findViewById(R.id.TVSignIn);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(ActivityLogin.this, "User logged in ", Toast.LENGTH_SHORT).show();
+                    Intent I = new Intent(ActivityLogin.this, UserActivity.class);
+                    startActivity(I);
+                } else {
+                    Toast.makeText(ActivityLogin.this, "Login to continue", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(ActivityLogin.this, MainActivity.class);
+                startActivity(I);
+            }
+        });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = loginEmailId.getText().toString();
+                String userPaswd = logInpasswd.getText().toString();
+                if (userEmail.isEmpty()) {
+                    loginEmailId.setError("Provide your Email first!");
+                    loginEmailId.requestFocus();
+                } else if (userPaswd.isEmpty()) {
+                    logInpasswd.setError("Enter Password!");
+                    logInpasswd.requestFocus();
+                } else if (userEmail.isEmpty() && userPaswd.isEmpty()) {
+                    Toast.makeText(ActivityLogin.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(userEmail.isEmpty() && userPaswd.isEmpty())) {
+                    firebaseAuth.signInWithEmailAndPassword(userEmail, userPaswd).addOnCompleteListener(ActivityLogin.this, new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(ActivityLogin.this, "Not sucessfull", Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(ActivityLogin.this, UserActivity.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(ActivityLogin.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
-}
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+

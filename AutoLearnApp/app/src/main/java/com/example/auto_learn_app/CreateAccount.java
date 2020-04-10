b78d221d0949cfb97baa1d688c27032557380bc8
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2020-04-10 11:36:34 -0500

    edit

--- AutoLearnApp/app/src/main/java/com/example/auto_learn_app/CreateAccount.java
+++ AutoLearnApp/app/src/main/java/com/example/auto_learn_app/CreateAccount.java
@@ -13,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
--- AutoLearnApp/app/src/main/java/com/example/auto_learn_app/LoginActivity.java
+++ AutoLearnApp/app/src/main/java/com/example/auto_learn_app/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.example.auto_learn_app;
 
 import android.content.Intent;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +8,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 

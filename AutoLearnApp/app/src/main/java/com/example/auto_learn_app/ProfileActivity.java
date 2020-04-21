ebb38260cf4352c50fca55b7d8680638f2582f73
driko-development <edrik.aguilera@mavs.uta.edu>
2020-04-20 20:03:55 -0500

    Porfile page

--- AutoLearnApp/app/src/main/java/com/example/auto_learn_app/ProfileActivity.java
+++ AutoLearnApp/app/src/main/java/com/example/auto_learn_app/ProfileActivity.java
@@ -1,12 +1,22 @@
 package com.example.auto_learn_app;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog.Builder;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity  {
     androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
@@ -19,17 +29,49 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Profile");
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile,menu);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // handle arrow click here
-        if (item.getItemId() == android.R.id.home) {
-            Intent loginIntent = new Intent(ProfileActivity.this, MainActivity.class);
+        switch (item.getItemId())
+        {
+            case R.id.home: {
+                Intent homeIntent = new Intent(ProfileActivity.this, MainActivity.class);
 
-            startActivity(loginIntent);
-            finish(); // close this activity and return to preview activity (if there is any)
+                startActivity(homeIntent);
+                finish(); // close this activity and return to preview activity (if there is any)
+                break;
+            }
+            case R.id.profile_number: {
+                Toast.makeText(this, "Name settings", Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.profile_email: {
+                Toast.makeText(this, "Email settings", Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.profile_password: {
+                Toast.makeText(this, "Password settings", Toast.LENGTH_SHORT).show();
+                break;
+            }
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }

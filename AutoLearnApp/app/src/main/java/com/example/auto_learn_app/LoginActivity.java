cf7f0a2e29a4387ce586147baf1f21d4eee3c5b4
driko-development <edrik.aguilera@mavs.uta.edu>
2020-04-27 17:28:20 -0500

    Fixed bug

--- AutoLearnApp/app/src/main/java/com/example/auto_learn_app/LoginActivity.java
+++ AutoLearnApp/app/src/main/java/com/example/auto_learn_app/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.auto_learn_app;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +13,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,7 +29,12 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     EditText email, password;
     private FirebaseAuth.AuthStateListener mListener;
-
+    // Defining Permission codes.
+    // We can give any value
+    // but unique for each permission.
+    private static final int CAMERA_PERMISSION_CODE = 100;
+    private boolean permissions_granted = false;
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,33 +65,37 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email2 = email.getText().toString();
-                String pass = password.getText().toString();
-                if (email2.isEmpty()){
-                    email.setError("Enter a email");
-                    email.requestFocus();
-                }
-                else if (pass.isEmpty()){
-                    password.setError("Enter a password");
-                    password.requestFocus();
-                }
-                else {
-                    mAuth.signInWithEmailAndPassword(email2,pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-                                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                if (permissions_granted) {
+                    String email2 = email.getText().toString();
+                    String pass = password.getText().toString();
+                    if (email2.isEmpty()) {
+                        email.setError("Enter a email");
+                        email.requestFocus();
+                    } else if (pass.isEmpty()) {
+                        password.setError("Enter a password");
+                        password.requestFocus();
+                    } else {
+                        mAuth.signInWithEmailAndPassword(email2, pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                                    Intent p = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(p);
+                                }
                             }
-                            else {
-                                Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
-                                Intent p = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(p);
-                            }
-                        }
-                    });
+                        });
+                    }
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Allow permissions to continue", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -98,6 +112,47 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mListener);
+        checkPermission(Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,CAMERA_PERMISSION_CODE);
+    }
+
+
+    // Function to check and request permission.
+    public void checkPermission(String permission1, String permission2, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(LoginActivity.this, permission1)
+                == PackageManager.PERMISSION_DENIED
+            && ContextCompat.checkSelfPermission(LoginActivity.this, permission2)
+                == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(LoginActivity.this, new String[] { permission1, permission2 }, requestCode);
+        }else { permissions_granted = true; }
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode)
+        {
+            case CAMERA_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(LoginActivity.this, "Permissions allowed", Toast.LENGTH_SHORT).show();
+                    permissions_granted = true;
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,
+                            "Permissions Denied",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                    permissions_granted = false;
+                }
+                break;
+            }
+        }
+
     }
     }
 
